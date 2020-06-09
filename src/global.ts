5fa5bbe777ffef5acadcaa8c22ec3c00f14c19f0
Sprax <developer@sprax2013.de>
2020-06-09 02:09:32 +0200

    Show skin's ai tags

--- src/global.ts
+++ src/global.ts
@@ -88,6 +88,7 @@ export interface SkinDBAccount {
 export interface SkinDBSkin {
   readonly skin: Skin;
   readonly tags: { id: string, name: string }[];
+  readonly aiTags: { id: string, name: string }[];
   readonly seen_on: { name: string, id: string }[];
 }
 
